d3486f47bf9897c497c5aa2b4ad7d4c7c96e6663
Mike Stephen Gollena <55428756+DJmistMAGO@users.noreply.github.com>
2023-09-13 01:05:52 +0800

    fixed annoucement table

--- resources/views/modules/announcement/index.blade.php
+++ resources/views/modules/announcement/index.blade.php
@@ -49,7 +49,7 @@
                                         </tr>
                                     @empty
                                         <tr>
-                                            <td colspan="3" class="text-center">No Data</td>
+                                            <td colspan="4" class="text-center">No Data</td>
                                         </tr>
                                     @endforelse
                                 </tbody>
